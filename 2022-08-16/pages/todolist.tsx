18cf7fbadac82ec3f162f852d1acf8135c3f29f3
ShinjungOh <tlswjd9260@gmail.com>
2022-08-18 02:23:48 +0900

    [ADD] TodoHeader 기능 추가

--- 2022-08-16/pages/todolist.tsx
+++ 2022-08-16/pages/todolist.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 
 import { TodoHeader } from '@components/todo';
 
-const Todolist = () => (
-  <div>
-    <TodoHeader title="할일" />
-  </div>
-);
+const Todolist = () => {
+  const today = new Date();
+
+  const dateString = today.toLocaleString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const dayName = today.toLocaleString('ko-KR', {
+    weekday: 'long',
+  });
+
+  return (
+    <div>
+      <TodoHeader dateString={dateString} dayName={dayName} undoneTaskLength={0} />
+    </div>
+  );
+};
 
 export default Todolist;
--- 2022-08-16/ui/components/todo/TodoHeader.tsx
+++ 2022-08-16/ui/components/todo/TodoHeader.tsx
@@ -2,30 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-  title: string;
+  dateString: string;
+  dayName: string;
+  undoneTaskLength: number;
 }
 
-const TodoHeader = ({ title }: Props) => (
+const TodoHeader = ({
+  dateString,
+  dayName,
+  undoneTaskLength,
+}: Props) => (
   <Container>
-    <Title>
-      {title}
-    </Title>
+    <Title>{dateString}</Title>
+    <Description>{dayName}</Description>
+    <UnDoneTask>할 일 {undoneTaskLength}개 남음</UnDoneTask>
   </Container>
-
 );
 
 export default TodoHeader;
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  padding: 24px;
+  justify-content: start;
+  padding: 42px 24px 24px 24px;
   box-sizing: content-box;
   height: 130px;
 `;
 
-const Title = styled.h3`
+const Title = styled.h1`
   font-size: 30px;
+  color: rgba(37, 36, 36, 0.81);
+  margin: 0;
+  font-weight: bold;
+`;
+
+const Description = styled.div`
+  font-size: 20px;
+  color: #6e6a6a;
+  margin-top: 5px;
+`;
+
+const UnDoneTask = styled.div`
+  font-size: 18px;
+  margin-top: 32px;
+  color: #3a68f9;
+  font-weight: bold;
 `;
