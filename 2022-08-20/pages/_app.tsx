68ddc79a2c3ffdfa6d0b22980259d6ca5cccae4c
ShinjungOh <tlswjd9260@gmail.com>
2022-08-20 17:50:59 +0900

    [ADD] 2nd: Layout 구현

--- 2022-08-20/pages/_app.tsx
+++ 2022-08-20/pages/_app.tsx
@@ -2,15 +2,21 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import GlobalStyles from '@ui/core/GlobalStyle';
+import { Page } from '@ui/components/layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps) {
   return (
     <>
       <Head>
         <title>투두 리스트</title>
       </Head>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <Page>
+        <Component {...pageProps} />
+      </Page>
     </>
   );
 }
---
+++ 2022-08-20/ui/components/layout/Layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => (
+  <Container>
+    {children}
+  </Container>
+);
+
+export default Layout;
+
+const Container = styled.div`
+  background-color: white;
+  width: 420px;
+  height: 900px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
