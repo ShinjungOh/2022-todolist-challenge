b3607866e8da9610702effa064c9a8605ec8bc50
ShinjungOh <tlswjd9260@gmail.com>
2022-09-03 23:55:50 +0900

    [ADD] 3rd: TodoItem 할 일 완료 체크 css 보완

--- 2022-08-28/ui/components/todo/TodoItem.tsx
+++ 2022-08-28/ui/components/todo/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { TodoItemType } from '../../../pages/todolist';
 
@@ -16,14 +16,14 @@ const TodoItem = ({
   text,
 }: Props & TodoItemType) => (
   <Container>
-    <DoneButton>
-      <DoneCheck done={done} onClick={() => onToggleDone(id)}>
+    <DoneButton done={done}>
+      <DoneCheck onClick={() => onToggleDone(id)}>
         {
           done && <img src="./icon/svgs/check.svg" alt="완료" />
         }
       </DoneCheck>
     </DoneButton>
-    <TodoText>{text}</TodoText>
+    <TodoText done={done}>{text}</TodoText>
     <DeleteButton onClick={() => onClickDelete(id)}>
       <img src="./icon/svgs/delete.svg" alt="삭제" />
     </DeleteButton>
@@ -43,24 +43,33 @@ const Container = styled.div`
   background-color: #edffff;
 `;
 
-const DoneButton = styled.div`
+const DoneButton = styled.div<{done: boolean}>`
   width: 30px;
   height: 30px;
   margin-right: 15px;
   border: 1px solid gray;
   border-radius: 50%;
   cursor: pointer;
+  ${(props) => props.done
+          && css`
+            border-color: #89abc6;
+          `}
 `;
 
-const DoneCheck = styled.div<{ done: boolean }>`
+const DoneCheck = styled.div`
   width: 25px;
   height: 25px;
   padding: 3px 0.5px 2px 3px;
 `;
 
-const TodoText = styled.text`
+const TodoText = styled.text<{ done: boolean }>`
   flex: 1;
   font-size: 20px;
+  ${(props) => props.done
+          && css`
+            color: gray;
+            text-decoration: line-through;
+          `}
 `;
 
 const DeleteButton = styled.div`
