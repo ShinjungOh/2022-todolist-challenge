18c061dc7e0ed2ee68200626d99516e7fead90d6
ShinjungOh <tlswjd9260@gmail.com>
2022-09-24 02:42:45 +0900

    [ADD] 5th: nextjs 초기 설정 추가

---
+++ 2022-09-23/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NextPage } from 'next';
+
+const Home: NextPage = () => (
+  <Container>
+    테스트
+  </Container>
+);
+
+export default Home;
+
+const Container = styled.div`
+  background-color: lightgoldenrodyellow;
+`;
