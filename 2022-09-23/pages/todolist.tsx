aa3b7a2e3133478fa78a4e89130f5ad342cd69f5
ShinjungOh <tlswjd9260@gmail.com>
2022-09-26 09:44:31 +0900

    [ADD] 5th: TodoHeader UI 구현

---
+++ 2022-09-23/pages/todolist.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { TodoHeader } from '@components/todo';
+
+const Todolist = () => (
+  <TodoHeader />
+);
+
+export default Todolist;
---
+++ 2022-09-23/ui/components/todo/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => (
+  <Container>
+    <DayContainer>
+      <Today>2022.09.26</Today>
+      <DayName>0요일</DayName>
+    </DayContainer>
+    <UndoneTodoLength>할 일이 0개 남았습니다.</UndoneTodoLength>
+  </Container>
+);
+
+export default Header;
+
+const Container = styled.div`
+  width: 100%;
+  height: 28%;
+  border-bottom: 1.5px solid #eaeaea;
+`;
+
+const DayContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 13px 15px;
+`;
+
+const Today = styled.div`
+  font-size: 25px;
+  color: #3d3c3c;
+`;
+
+const DayName = styled.div`
+  font-size: 20px;
+  color: #a6a5a5;
+`;
+
+const UndoneTodoLength = styled.div`
+  width: 100%;
+  height: 60%;
+  padding: 10px 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 23px;
+  color: #4b4b4b;
+`;
