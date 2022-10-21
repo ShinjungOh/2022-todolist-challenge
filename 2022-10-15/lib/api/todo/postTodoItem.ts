378b0af8140d13f49dc506d4e5d0e26f879bf8cc
ShinjungOh <tlswjd9260@gmail.com>
2022-10-22 04:44:05 +0900

    [UPDATE] 8th: postTodoItem api 수정

--- 2022-10-15/lib/api/todo/postTodoItem.ts
+++ 2022-10-15/lib/api/todo/postTodoItem.ts
@@ -1,10 +1,13 @@
 import { client } from '../client';
 
-export const postTodoItem = async (param: string) => {
+export const postTodoItem = async (text: string) => {
   const url = '/api/v2/todo';
   const userToken = localStorage.getItem('token');
   const headers = {
     authorization: `Bearer ${userToken}`,
+  };
+  const param = {
+    text,
   };
   await client.post(url, param, { headers });
 };
