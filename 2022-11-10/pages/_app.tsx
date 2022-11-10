4d14ae3af55184a6ca09afa4c3c110e9dbe71117
ShinjungOh <tlswjd9260@gmail.com>
2022-11-10 23:32:11 +0900

    [ADD] 9th: nextjs 초기 설정 추가

---
+++ 2022-11-10/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from '@ui/core/GlobalStyles';
+
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps) => (
+  <>
+    <Head>
+      <title>투두 리스트</title>
+    </Head>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
---
+++ 2022-11-10/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Home: NextPage = () => (
+  <Container>
+    todolist
+  </Container>
+);
+
+export default Home;
+
+const Container = styled.div`
+  background-color: #ffd500;
+`;
